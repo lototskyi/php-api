a80c2f6fefd5fa3223efbe6796c7573d6da190da
alex <o.lototskyi@gmail.com>
2023-01-12 10:01:35 +0200

    basic CURL options

--- public/index.php
+++ public/index.php
@@ -6,12 +6,15 @@ $ch = curl_init();
 //curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 curl_setopt_array($ch, [
-    CURLOPT_URL => "https://randomuser.me/api",
-    CURLOPT_RETURNTRANSFER => true
+    CURLOPT_URL => "https://api.openweathermap.org/data/2.5/weather?q=Kyiv",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HEADER => true
 ]);
 
 $response = curl_exec($ch);
 
-curl_close($ch);
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
+curl_close($ch);
+echo $status_code . "\n";
 echo $response . "\n";
