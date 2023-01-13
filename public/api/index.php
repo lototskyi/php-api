e1693a31e94bdc6f85d41679d1c66436cf79dff0
alex <o.lototskyi@gmail.com>
2023-01-13 12:02:53 +0200

    error handler, json output

--- public/api/index.php
+++ public/api/index.php
@@ -1,5 +1,9 @@
 <?php
+declare(strict_types=1);
+
 require dirname(__DIR__) . '/vendor/autoload.php';
+
+set_exception_handler("ErrorHandler::handleException");
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $parts = explode('/', $path);
@@ -12,6 +16,8 @@ if ($resource !== 'tasks') {
     http_response_code(404);
     exit;
 }
+
+header("Content-type: application/json; charset=UTF-8");
 
 $controller = new TaskController();
 $controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
