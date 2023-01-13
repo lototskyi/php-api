2fa19e9d536bb682873cd9efd8f626a6436add35
alex <o.lototskyi@gmail.com>
2023-01-13 10:31:25 +0200

    change response code (404)

---
+++ public/api/index.php
@@ -0,0 +1,17 @@
+<?php
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$parts = explode('/', $path);
+
+$resource = $parts[2];
+$id = $parts[3] ?? null;
+
+if ($resource !== 'tasks') {
+    //header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
+    http_response_code(404);
+    exit;
+}
+
+echo $resource . ', ' . $id;
+
+echo $_SERVER['REQUEST_METHOD'];
+
