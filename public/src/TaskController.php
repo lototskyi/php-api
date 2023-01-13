001cdff7392bb820906f71b34c59bf87bc68bdf3
alex <o.lototskyi@gmail.com>
2023-01-13 21:09:17 +0200

    change settings to not convert values to string, get one task, convert is_completed to boolean

--- public/src/TaskController.php
+++ public/src/TaskController.php
@@ -17,9 +17,16 @@ readonly class TaskController
                 $this->respondMethodNotAllowed("GET, POST");
             }
         } else {
+            $task = $this->taskGateway->get($id);
+
+            if ($task === false) {
+                $this->respondNotFound($id);
+                return;
+            }
+
             switch ($method) {
                 case 'GET':
-                    echo "show $id";
+                    echo json_encode($task);
                     break;
                 case 'PATCH':
                     echo "update $id";
@@ -37,5 +44,11 @@ readonly class TaskController
     {
         http_response_code(405);
         header("Allow: $allowed_methods");
+    }
+
+    private function respondNotFound(string $id): void
+    {
+        http_response_code(404);
+        echo json_encode(["message" => "Task with ID $id not found"]);
     }
 }
--- public/src/TaskGateway.php
+++ public/src/TaskGateway.php
@@ -15,6 +15,31 @@ class TaskGateway
                 FROM task
                 ORDER BY name";
         $stmt = $this->conn->query($sql);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $data = [];
+
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $row['is_completed'] = (bool) $row['is_completed'];
+            $data[] = $row;
+        }
+
+        return $data;
+    }
+
+    public function get(string $id): array | false
+    {
+        $sql = "SELECT * 
+                FROM task
+                WHERE id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($data !== false) {
+            $data['is_completed'] = (bool) $data['is_completed'];
+        }
+
+        return $data;
     }
 }
