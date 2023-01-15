eca9b2867e1f653defec6885acd50731e56279b2
alex <o.lototskyi@gmail.com>
2023-01-15 13:27:51 +0200

    handle errors

--- public/src/ErrorHandler.php
+++ public/src/ErrorHandler.php
@@ -2,6 +2,16 @@
 
 class ErrorHandler
 {
+    public static function handleError(
+        int $errno,
+        string $errstr,
+        string $errfile,
+        string $errline
+    ): void
+    {
+        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    }
+
     public static function handleException(Throwable $exception): void
     {
         http_response_code(500);
