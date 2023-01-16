ecb656ca255cad6880e3f532da5d5196ce1789cc
alex <o.lototskyi@gmail.com>
2023-01-16 10:14:20 +0200

    adding users, generating API key

--- docker-phpapi/database/init.sql
+++ docker-phpapi/database/init.sql
@@ -11,3 +11,14 @@ VALUES
     ('Buy new shoes', 1, true),
     ('Renew passport', 2, false),
     ('Paint wall', NULL, true);
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+    username VARCHAR(128) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    api_key VARCHAR(32) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (username),
+    UNIQUE (api_key)
+);
